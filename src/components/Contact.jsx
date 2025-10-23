bc9c4e8d6ca9c1b6257738895283e3c30111d947
Kashinath <rpkashinath@gmail.com>
2025-10-23 16:07:09 +0530

    Update contact information and address in Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,13 +39,13 @@ const Contact = () => {
 		
 		<div>
 		 <h3>Visit Our Bar</h3>
-		 <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+		 <p>123 Velvet Way, Suite 101, Los Angeles, CA 90069</p>
 		</div>
 		
 		<div>
 		 <h3>Contact Us</h3>
-		 <p>(555) 987-6543</p>
-		 <p>hello@jsmcocktail.com</p>
+		 <p>(213) 555-1234</p>
+		 <p>reservations@velvetpour.com</p>
 		</div>
 		
 		<div>
